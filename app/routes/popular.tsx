2c4cf28df3f5c79b6362b6c7e67381c539585e20
STHEXA <ti.syu.s@gmail.com>
2025-05-09 11:11:25 +0900

    ルーティング基本設定

---
+++ app/routes/popular.tsx
@@ -0,0 +1,9 @@
+export default function popular() {
+  return (
+    <div>
+      <div className="flex-1 sm:ml-64">
+        <h1>人気記事</h1>
+      </div>
+    </div>
+  );
+}
---
+++ app/routes/search.tsx
@@ -0,0 +1,9 @@
+export default function search() {
+  return (
+    <div>
+      <div className="flex-1 sm:ml-64">
+        <h1>記事検索</h1>
+      </div>
+    </div>
+  );
+}
